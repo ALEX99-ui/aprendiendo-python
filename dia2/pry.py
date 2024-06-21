de5ffe1a923b57a4bad43e0e0fffaaed1fa360a2
AlexUrrea <urreaalex27@gmail.com>
2024-06-21 13:42:04 -0500

    correcion

--- dia2/pry.py
+++ dia2/pry.py
@@ -1,8 +1,9 @@
 nombre = input("cual es tu nombre?")
 print("Hola " + nombre)
+
 venta = int(input("cuanto has vendido este mes?"))
 
-comision = (venta * 13)/100
-comision = round(comision)
+comision = round(venta * 13/ 100)
+
 print(f"hola {nombre}, tus comisiones de este mes es ${comision}")
 
