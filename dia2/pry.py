51de5cd3018652ca6a54bb3657db740534bf1749
AlexUrrea <urreaalex27@gmail.com>
2024-06-21 13:37:55 -0500

    cambio

--- dia2/pry.py
+++ dia2/pry.py
@@ -1,10 +1,8 @@
-# solicita al usuario ingresar su nombre
 nombre = input("cual es tu nombre?")
 print("Hola " + nombre)
-
-# pregunta al usuario cuanto ha vendido este mes
 venta = int(input("cuanto has vendido este mes?"))
 
 comision = (venta * 13)/100
-print(nombre + " tu comision de venta es:  " + str(comision))
+comision = round(comision)
+print(f"hola {nombre}, tus comisiones de este mes es ${comision}")
 
